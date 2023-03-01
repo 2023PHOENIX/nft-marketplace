21200629b90187b30971a758298ace035a8fb7a0
abhishek <abhishekshrivastav1920@gmail.com>
2023-03-01 13:31:11 +0530

    fontawesome icons update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
-
-import Navbar from "@/Components/Navbar";
-import Footer from "@/Components/Footer";
+import Navbar from "../Components/Navbar.jsx";
+import Footer from "../Components/Footer.jsx";
 
 import "../styles/globals.css";
 
@@ -14,6 +14,9 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
         <Footer />
       </div>
+
+      {/* NOTE: fontawesome icons */}
+      <Script src="https://kit.fontawesome.com/6ebc7a05ae.js" crossorigin="anonymous"></Script>
     </ThemeProvider>
   );
 }
