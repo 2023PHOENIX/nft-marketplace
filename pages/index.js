c8fdcaa4bbd8c73c2eb126791c1d1f0cc73dfdab
abhishek <abhishekshrivastav1920@gmail.com>
2023-03-02 12:08:56 +0530

    basic navbar setted with routing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="text-3xl underline bg-red-200"> Hello world</div>
+    <div className="text-3xl underline bg-red-200"></div>
   )
 }
