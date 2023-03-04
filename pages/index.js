ee9ad5e91a9055d8a8e6e7c5779c5bb75662dce8
abhishek <abhishekshrivastav1920@gmail.com>
2023-03-04 10:02:43 +0530

    top seller added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 
 export default function Home() {
   return (
-    <div className="text-3xl underline bg-red-200"></div>
+    <div className="text-3xl underline bg-red-200">
+
+
+
+
+      {/* TOP SELLER */}
+      <div> 
+        <h1> Top Seller</h1>
+      </div>
+    </div>
   )
 }
