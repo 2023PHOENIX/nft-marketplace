7308e0d6e0359ba3b9b2abeb0be793152ddd5519
abhishek <abhishekshrivastav1920@gmail.com>
2023-03-04 10:10:03 +0530

    conflict resolved

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Banner from "../Components/Banner.jsx";
 
 export default function Home() {
   return (
-    <div className="text-3xl underline bg-red-200">
-
-
-
-
-
+    <div className="flex justify-center sm:px-4 p-12 xl:mx-24">
+      <div className="w-full minwd:w-4/5">
+        <Banner
+          textContent="Buy and Sell NFTs from all over the world"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl  text-left"
+        />
+      </div>
     </div>
-  )
+  );
 }
